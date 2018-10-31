3e01d311e307974c2bb4c178df7a8cc7b270017e
Wyatt Ades <wyattades@gmail.com>
2018-10-31 13:05:29 -0700

    Update tests

--- __test__/src/index.js
+++ __test__/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
+import * as Offline from 'offline-plugin/runtime';
 
 import './style.scss';
 import App from './App';
 import TestWorker from './test.worker.js';
 
+
+// Enable offline plugin: uses a service worker to cache resources
+Offline.install();
+
 require.context('./static', true);
 
 const testWorker = new TestWorker();
-
 
 render(<App/>, document.getElementById('root'));
--- __test__/webpack.config.js
+++ __test__/webpack.config.js
@@ -1,12 +1,13 @@
 module.exports = require('../index')({
   react: true,
+  manifest: {
+    start_url: 'https://localhost:8080',
+    background_color: '#FF0000',
+  },
   pages: [{
     title: 'Test Page',
     meta: {
       'theme-color': '#3367D6'
-    },
-    manifest: {
-      background_color: '#FF0000',
     },
     headElements: [{ tag: 'script', src: 'not_found.js' }],
   }],
