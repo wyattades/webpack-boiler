c4848d09293e1dd786e1978b33222588e06d5a5e
Wyatt Ades <wyattades@gmail.com>
2019-01-16 19:27:50 -0800

    update tests

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -1,7 +1,6 @@
 module.exports = require('../index')({
   react: true,
   manifest: {
-    start_url: 'https://localhost:8080',
     background_color: '#FF0000',
   },
   entry: {
@@ -9,10 +8,14 @@ module.exports = require('../index')({
     bar: './src/anotherEntryPoint.js',
   },
   output: 'myBuildDirectory',
+  devPort: 8000,
+  url: 'https://example.com',
   pages: [{
     title: 'Test Page',
+    favicon: './src/favicon.png',
     meta: {
-      'theme-color': '#3367D6'
+      'theme-color': '#3367D6',
+      description: 'This is a great website',
     },
     headElements: [{ tag: 'script', src: 'not_found.js' }],
   }],
